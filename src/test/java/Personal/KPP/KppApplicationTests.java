622a1492c499a8d3f817a7c5ef9b5752c99fe846
DeokhunKim <koon0312@naver.com>
2022-02-08 21:12:24 +0900

    Load spring boot

---
+++ src/test/java/Personal/KPP/KppApplicationTests.java
@@ -0,0 +1,13 @@
+package Personal.KPP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
